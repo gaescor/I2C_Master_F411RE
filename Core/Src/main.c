00a8c3905897896bc134ce580469dfe8211ff474
Gustavo Escobar <gaescor@gmail.com>
2025-09-16 13:59:28 -0500

    Fix: integración de I2C_ClearBusyFlagErratum y correcciones en interrupción PC13

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,64 +1,193 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body - I2C Master con pulsador en PC13
+  * @author         : Gustavo Escobar
+  * @version        : v1.0
+  * @date           : 11/09/2025
+  ******************************************************************************
+  * @attention
+  * Este código fue generado con CubeMX y adaptado para pruebas de I2C Master
+  * con STM32F411RE. Incluye integración de un pulsador en PC13 para iniciar
+  * la transmisión I2C.
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
+/* USER CODE BEGIN PV */
+volatile uint8_t buttonPressed = 0; // bandera global
+
 #define SLAVE_ADDRESS (0x30 << 1) // Dirección 7 bits, desplazada a 8 bits
 #define TIMEOUT 100
+/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART2_UART_Init(void);
-void Error_Handler(void);
+/* USER CODE BEGIN PFP */
+void WaitForButtonPress(void);
+/* USER CODE END PFP */
 
-/* Dirección del esclavo en formato 8 bits */
-#define SLAVE_ADDR (0x30 << 1)
+/* USER CODE BEGIN 0 */
+// Callback que se ejecuta cuando hay una interrupción EXTI
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if (GPIO_Pin == GPIO_PIN_13)
+    {
+        buttonPressed = 1;
+        char msg[] = "Interrupcion recibida en PC13\r\n";
+        HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+    }
+}
 
-/* MAIN ----------------------------------------------------------------------*/
+void WaitForButtonPress(void)
+{
+
+
+    // Espera hasta que la interrupción marque la bandera
+    while (!buttonPressed)
+    {
+        // se queda en espera
+    	char msg[] = "Sistema inicializado.\r\nPresione el pulsador para iniciar la transmision I2C...\r\n";
+    	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+    	HAL_Delay(3000);
+    }
+
+    char msg2[] = "Pulsador detectado, iniciando transmision I2C...\r\n";
+    HAL_UART_Transmit(&huart2, (uint8_t*)msg2, strlen(msg2), HAL_MAX_DELAY);
+
+    HAL_Delay(200); // anti-rebote
+}
+
+
+void I2C_ClearBusyFlagErratum(I2C_HandleTypeDef *hi2c)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    // 1. Resetear y deshabilitar I2C
+    __HAL_RCC_I2C1_FORCE_RESET();
+    __HAL_RCC_I2C1_RELEASE_RESET();
+    __HAL_RCC_I2C1_CLK_DISABLE();
+
+    // 2. Configurar PB6 (SCL) y PB7 (SDA) como GPIO open-drain
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    GPIO_InitStruct.Pin = GPIO_PIN_6; // SCL
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_7; // SDA
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    // 3. Generar 9 pulsos en SCL
+    for (int i = 0; i < 9; i++)
+    {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+        HAL_Delay(1);
+    }
+
+    // 4. Generar STOP manual
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET); // SDA alto
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET); // SCL alto
+    HAL_Delay(1);
+
+    // 5. Restaurar configuración de pines a función I2C
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+
+    GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    // 6. Habilitar reloj de nuevo
+    __HAL_RCC_I2C1_CLK_ENABLE();
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-  uint8_t tx = 'A';
+  /* USER CODE BEGIN 1 */
+  uint8_t data[2] = {'A', 'B'};
+  char msg[50];
+  /* USER CODE END 1 */
 
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
+  /* Configure the system clock */
   SystemClock_Config();
+
+  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
 
+  strcpy(msg, "Inicio Master I2C\r\n");
+  HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
 
-  uint8_t data[2] = {'A', 'B'};
-  char msg[50];
-  /* Mensaje inicial por UART */
-  //msg[] = "Inicio Master I2C\r\n";
-  HAL_UART_Transmit(&huart2, (uint8_t*)msg, sizeof(msg)-1, HAL_MAX_DELAY);
+  HAL_Delay(2000);
 
+  // Esperar a que el usuario presione el botón antes de entrar al while
+  WaitForButtonPress();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
-	    HAL_StatusTypeDef res = HAL_I2C_Master_Transmit(&hi2c1, SLAVE_ADDRESS, data, 2, TIMEOUT);
+    HAL_StatusTypeDef res = HAL_I2C_Master_Transmit(&hi2c1, SLAVE_ADDRESS, data, 2, TIMEOUT);
 
-	    if (res == HAL_OK) {
-	      sprintf(msg, "TX OK: %c %c\r\n", data[0], data[1]);
-	      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-	    } else {
-	      sprintf(msg, "Error en TX I2C: %d\r\n", res);
-	      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-	    }
+    if (res == HAL_OK) {
+      sprintf(msg, "TX OK: %c %c\r\n", data[0], data[1]);
+      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+    } else {
+      sprintf(msg, "Error en TX I2C: %d\r\n", res);
+      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+    }
 
-	    HAL_Delay(1000);
-
+    HAL_Delay(1000);
   }
-}
+  /* USER CODE END WHILE */
 
-/* -------------------------------------------------------------------------- */
-/* Configuración de I2C1 como Master */
+  /* USER CODE BEGIN 3 */
+}
+/* USER CODE END 3 */
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_I2C1_Init(void)
 {
+  /* USER CODE BEGIN I2C1_Init 0 */
+	 I2C_ClearBusyFlagErratum(&hi2c1);
+  /* USER CODE END I2C1_Init 0 */
+
   hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;             // 100 kHz
+  hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -70,12 +199,22 @@ static void MX_I2C1_Init(void)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
 }
 
-/* -------------------------------------------------------------------------- */
-/* Configuración de USART2 para debug */
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_USART2_UART_Init(void)
 {
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -88,10 +227,47 @@ static void MX_USART2_UART_Init(void)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 }
 
-/* -------------------------------------------------------------------------- */
-/* Configuración mínima del reloj */
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  /* USER CODE BEGIN MX_GPIO_Init_0 */
+  /* USER CODE END MX_GPIO_Init_0 */
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* Configurar PC13 como entrada con interrupción (Botón USER) */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* Habilitar EXTI Line interrupt */
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/**
+  * @brief  System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -121,27 +297,10 @@ void SystemClock_Config(void)
   }
 }
 
-/* -------------------------------------------------------------------------- */
-/* GPIO */
-static void MX_GPIO_Init(void)
-{
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* LED de usuario en PC13 (opcional para debug) */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-}
-
-/* -------------------------------------------------------------------------- */
-/* Error_Handler */
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
   __disable_irq();
@@ -151,3 +310,15 @@ void Error_Handler(void)
     HAL_Delay(200);
   }
 }
+
+/* USER CODE BEGIN 4 */
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_EndOfFile */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Fin de archivo main
+  ******************************************************************************
+  */
+/* USER CODE END Header_EndOfFile */
